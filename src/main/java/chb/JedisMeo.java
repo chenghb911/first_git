8292a5bb12ad98209581e0bec8a686b97766bb03
Tom <1739104568@qq.com>
2021-10-23 17:25:37 +0800

    first commit

---
+++ src/main/java/chb/JedisMeo.java
@@ -0,0 +1,12 @@
+package chb;
+
+import redis.clients.jedis.Jedis;
+
+public class JedisMeo {
+    public static void main(String[] args) {
+        Jedis jedis = new Jedis("192.168.216.128",6379);
+        jedis.auth("cc15907007189");
+        String str = jedis.ping();
+        System.out.println(str);
+    }
+}
